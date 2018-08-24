b3918f0a1925b77971bdb2e94b886850caceb504
amir <amirgraitzer@gmail.com>
2018-08-24 08:30:03 +0300

    Media admin + static folder

--- events/admin.py
+++ events/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from events.models import Event, EventCategory, EventSubCategory, Venue, Artist, EventPromotion, UserSwipeAction
+from events.models import Event, EventCategory, EventSubCategory, Venue, Artist, EventPromotion, UserSwipeAction, Media
 
 admin.site.register(Event)
 admin.site.register(EventCategory)
@@ -10,4 +10,5 @@ admin.site.register(Venue)
 admin.site.register(Artist)
 admin.site.register(EventPromotion)
 admin.site.register(UserSwipeAction)
+admin.site.register(Media)
 
