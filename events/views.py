3ac43201007646e4a8e1de097f3df6b1cc6c12ae
amir <amirgraitzer@gmail.com>
2018-08-26 10:39:43 +0300

    absolute artist image url + fix artist last name field name

--- events/views.py
+++ events/views.py
@@ -25,8 +25,8 @@ def get_events(request):
                   'endTime': event.end_time.strftime("%d.%m - %H:%M"),
                   'venue': {'name': event.venue.name, 'streetAddress': event.venue.street_address,
                             'city': event.venue.city},
-                  'artist': {'firstName': event.artist.first_name, 'lastName': event.artist.lastName,
-                             'image': event.artist.image.url,
+                  'artist': {'firstName': event.artist.first_name, 'lastName': event.artist.last_name,
+                             'image': request.build_absolute_uri(event.artist.image.url),
                              'media': [{'type': m.type, 'link': m.link} for m in
                                        event.artist.media.all()]} if event.artist else None,
                   'media': [{'type': m.type, 'link': m.link} for m in event.media.all()]
