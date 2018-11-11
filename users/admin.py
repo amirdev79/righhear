f6e2ec225e27d26599b8569243c41894b0c74630
amir <amirgraitzer@gmail.com>
2018-11-11 14:17:50 +0200

    add user device model to admin

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from users.models import UserProfile
+from users.models import UserProfile, UserDevice
 
 admin.site.register(UserProfile)
+admin.site.register(UserDevice)
