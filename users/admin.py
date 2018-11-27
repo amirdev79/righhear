6ff156acf91e69f140f05207364234892989cb8f
amir <amirgraitzer@gmail.com>
2018-11-27 11:53:47 +0200

    UserSwipeAction admin format

--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,11 @@ from users.models import UserProfile, UserDevice, UserSwipeAction
 class UserDeviceAdmin(admin.ModelAdmin):
     list_display = ('user', 'device_id', 'os', 'os_version', 'model', 'timezone', 'last_login')
 
+
+class UserSwipeActionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'event', 'action')
+
 admin.site.register(UserProfile)
 admin.site.register(UserDevice, UserDeviceAdmin)
-admin.site.register(UserSwipeAction)
+admin.site.register(UserSwipeAction, UserSwipeActionAdmin)
 
