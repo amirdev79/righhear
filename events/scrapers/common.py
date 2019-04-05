f6b950cb22331bbd15e8e48200e482c72aff6066
amir <amirgraitzer@gmail.com>
2019-04-05 13:04:30 +0300

    artists csv export & import

--- events/scrapers/common.py
+++ events/scrapers/common.py
@@ -8,13 +8,11 @@ ARTIST_CSV_HEADER = 'id (-), first_name, last_name, first_name_heb, last_name_he
                     'media2_link, media2_playback_start, media2_playback_end, media3_id, media3_type, media3_link, ' \
                     'media3_playback_start, media3_playback_end '
 
-ARTIST_FIELDS = ['id', 'first_name', 'last_name', 'first_name_heb', 'last_name_heb', 'category', 'sub_categories',
-                 'media1 id', \
-                 'media1 type', 'media1 link', 'media1 playback start', 'media1 playback end', 'media2 id',
-                 'media2 type', \
-                 'media2 link', 'media2 playback start', 'media2 playback end', 'media3 id', 'media3 type',
-                 'media3 link' \
-                 'media3 playback start', 'media3 playback end']
+ARTIST_FIELDS = ['id', 'first_name', 'last_name', 'first_name_heb', 'last_name_heb', 'image', 'category',
+                 'sub_categories', 'media1 id', 'media1 type', 'media1 link', 'media1 playback start',
+                 'media1 playback end', 'media2 id', 'media2 type', 'media2 link', 'media2 playback start',
+                 'media2 playback end', 'media3 id', 'media3 type', 'media3 link', 'media3 playback start',
+                 'media3 playback end']
 
 MEDIA_TYPES = {v: k for k, v in Media.MEDIA_TYPE_CHOICES.items()}
 
@@ -38,10 +36,10 @@ def _get_artist_image_str(artist):
 def _get_artist_csv_line(artist):
     image = _get_artist_image_str(artist)
     category = artist.category.id if artist.category else ''
-    sub_categories = artist.sub_categories.values_list('id', flat=True) or ''
+    sub_categories = ','.join([str(id) for id in artist.sub_categories.values_list('id', flat=True) or []])
 
     fields = [str(artist.id), artist.first_name, artist.last_name, artist.first_name_heb, artist.last_name_heb, image,
-              category, sub_categories]
+              str(category), sub_categories]
     for media in artist.media.all():
         fields += _get_media_fields(media)
 
@@ -49,7 +47,7 @@ def _get_artist_csv_line(artist):
 
 
 def artists_to_csv(csv_path):
-    artists = [_get_artist_csv_line(artist) for artist in Artist.objects.all()]
+    artists = [_get_artist_csv_line(artist) for artist in Artist.objects.order_by('id')]
     with open(csv_path, 'w+') as f:
         f.write(','.join(ARTIST_FIELDS) + '\n')
         f.write('\n'.join(artists))
@@ -109,23 +107,48 @@ def create_new_artist(fields):
 
 
 def _update_existing_artist_media(artist, media_fields):
-    media1_id, media1_type, media1_link, media1_playback_start, media1_playback_end, media2_id, media2_type, media2_link, media2_playback_start, media2_playback_end, media3_id, media3_type, media3_link, media3_playback_start, media3_playback_end = media_fields
-    artist.media.clear()
-    artist.media.add(*[int(m) for m in [media1_id, media2_id, media3_id] if m])
+    if len(media_fields) == 0:
+        return
+    else:
+        artist.media.clear()
+        if len(media_fields) >= 5:
+            media1_id, media1_type, media1_link, media1_playback_start, media1_playback_end = media_fields[:5]
+            if media1_id != '':
+                artist.media.add(int(media1_id))
+        if len(media_fields) >= 10:
+            media2_id, media2_type, media2_link, media2_playback_start, media2_playback_end =   media_fields[5:10]
+            if media2_id != '':
+                artist.media.add(int(media2_id))
+        if len(media_fields) >= 15:
+            media3_id, media3_type, media3_link, media3_playback_start, media3_playback_end = media_fields[10:15]
+            if media3_id != '':
+                artist.media.add(int(media3_id))
 
 
 def _update_artist_category(artist, category_id):
+    if category_id == '':
+        raise Exception('Artist ' + str(artist) + ' did not get a category id. quitting..')
     artist.category = EventCategory.objects.get(id=int(category_id))
 
 
 def _update_artist_sub_categories(artist, sub_categories_ids):
+    if sub_categories_ids == '':
+        raise Exception('Artist ' + str(artist) + ' did not get any sub categories ids. quitting..')
     artist.sub_categories.clear()
+    sc_ids = sub_categories_ids.split(',')
     artist.sub_categories.add(*[int(sc) for sc in sub_categories_ids.split(',')])
 
 
 def _update_existing_artist(fields):
-    id, first_name, last_name, first_name_heb, last_name_heb, category_id, sub_categories_ids = fields[:7]
-    media_fields = fields[7:]
+    id, first_name, last_name, first_name_heb, last_name_heb, image, category_id, sub_categories_ids = fields[:8]
+    print ('first: '  +first_name)
+    print ('last_name: '  +last_name)
+    print ('first_name_heb: '  +first_name_heb)
+    print ('last_name_heb: '  +last_name_heb)
+    print ('image: '  +image)
+    print ('category_id: '  +category_id)
+    print ('sub_categories_ids: '  +sub_categories_ids)
+    media_fields = fields[8:]
     artist = Artist.objects.get(id=id)
     artist.first_name = first_name
     artist.last_name = last_name
