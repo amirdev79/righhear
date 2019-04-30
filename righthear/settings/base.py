71b339e9f173aee58ad68d7410d9c1ae7d2ee50e
amir <amirgraitzer@gmail.com>
2019-04-30 11:44:01 +0300

    django production settings

--- righthear/settings/base.py
+++ righthear/settings/base.py
@@ -31,7 +31,10 @@ STATICFILES_DIRS = (PROJECT_DIR + '/static',)
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*+g11=6$#96vx%8h+^h^bv3ot7!+-#(3gxne7vfm*#0*)o$_-5'
+# SECRET_KEY = '*+g11=6$#96vx%8h+^h^bv3ot7!+-#(3gxne7vfm*#0*)o$_-5'
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '*+g11=6$#96vx%8h+^h^bv3ot7!+-#(3gxne7vfm*#0*)o$_-5')
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
